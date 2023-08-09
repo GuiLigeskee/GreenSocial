b1ba20188bab140ccd32c2e63ef12d011272e72d
GuiLigeskee <112131340+GuiLigeskee@users.noreply.github.com>
2023-08-09 18:24:19 -0300

    atualização dia 09/08/23
    Problemas com token para utilização do formulário do editProfile.jsx;

--- ThorPhed/src/routes/EditProfile/EditProfile.jsx
+++ ThorPhed/src/routes/EditProfile/EditProfile.jsx
@@ -1,7 +1,38 @@
 import './EditProfile.css';
 
+import {uploads} from '../../utils/config';
+
+// Hooks
+import {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+// Redux
+import {profile, resetMessage} from '../../slices/userSlice';
+
+// Components
+import Message from '../../components/Messages/Message';
 
 const EditProfile = () => {
+
+  const dispatch = useDispatch();
+
+  const {user, message, error, loading} = useSelector((state) => state.user);
+
+  // states
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setImageProfile] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+
+  // Load user data
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Fill form with user data
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -13,19 +44,19 @@ const EditProfile = () => {
       <p className="subtitle">Adicione sua linda imagem de perfil e conte mais sobre você.</p>
       {/* preview da imagem */}
       <form onSubmit={handleSubmit}>
-        <input type="text"placeholder='NickName' />
-        <input type="email" placeholder='E-mail' disabled />
+        <input type="text"placeholder='NickName' onChange={(e) => setName(e.target.value)} value={name||""}/>
+        <input type="email" placeholder='E-mail' disabled value={email||""}/>
         <label>
           <span>Imagem do perfil:</span>
           <input type="file" />
         </label>
         <label>
           <span>Biografia:</span>
-          <input type="text" placeholder='Descrição do perfil' />
+          <input type="text" placeholder='Descrição do perfil' onChange={(e) => setBio(e.target.value)} value={bio||""} />
         </label>
         <label>
           <span>Quer trocar sua senha?</span>
-          <input type="password" placeholder='Digite sua nova senha' />
+          <input type="password" placeholder='Digite sua nova senha' onChange={(e) => setPassword(e.target.value)} value={password||""} />
         </label>
         <input type="submit" value="Atualizar" />
       </form>
